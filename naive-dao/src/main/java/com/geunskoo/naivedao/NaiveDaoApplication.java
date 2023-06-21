12bf827ba8ea339c2d15ad9d54b9f8918e063f39
geunskoo <oryukdo3@pusan.ac.kr>
2023-06-21 11:28:45 +0900

    init: 초난감 dao 실습파일 생성

---
+++ naive-dao/src/main/java/com/geunskoo/naivedao/NaiveDaoApplication.java
@@ -0,0 +1,13 @@
+package com.geunskoo.naivedao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiveDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaiveDaoApplication.class, args);
+    }
+
+}
---
+++ naive-dao/src/test/java/com/geunskoo/naivedao/NaiveDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geunskoo.naivedao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiveDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
