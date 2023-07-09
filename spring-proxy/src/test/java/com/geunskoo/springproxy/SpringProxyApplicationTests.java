a877a8b4e47bb87f4972cd5dfd436cb869a8ad5a
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-10 00:07:42 +0900

    init: 프록시 공부를 위한 스프링 부트 파일 생성

---
+++ spring-proxy/src/test/java/com/geunskoo/springproxy/SpringProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geunskoo.springproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
