fcd9d15906487660ad1c36344f973300abc1a049
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-10 17:08:52 +0900

    fix: 로그 추적기 빈 등록 및 빈 스캔 패키지 경로 설정

--- spring-proxy/src/main/java/com/geunskoo/springproxy/SpringProxyApplication.java
+++ spring-proxy/src/main/java/com/geunskoo/springproxy/SpringProxyApplication.java
@@ -2,17 +2,26 @@ package com.geunskoo.springproxy;
 
 import com.geunskoo.springproxy.app.config.AppV1Config;
 import com.geunskoo.springproxy.app.config.AppV2Config;
+import com.geunskoo.springproxy.app.config.v1_proxy.InterfaceProxyConfig;
+import com.geunskoo.springproxy.trace.logtrace.LogTrace;
+import com.geunskoo.springproxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
-@SpringBootApplication(scanBasePackages = "com.geunskoo.springproxy.app")
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "com.geunskoo.springproxy.app.interface_proxy")
 public class SpringProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProxyApplication.class, args);
     }
 
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
 }
