f9a9800867d3160114ddd29a046263d26048a550
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-10 14:19:21 +0900

    feat: v1, v2 객체 의존관계 구성파일 생성, 수동 빈 등록

--- spring-proxy/src/main/java/com/geunskoo/springproxy/SpringProxyApplication.java
+++ spring-proxy/src/main/java/com/geunskoo/springproxy/SpringProxyApplication.java
@@ -1,9 +1,14 @@
 package com.geunskoo.springproxy;
 
+import com.geunskoo.springproxy.app.config.AppV1Config;
+import com.geunskoo.springproxy.app.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "com.geunskoo.springproxy.app")
 public class SpringProxyApplication {
 
     public static void main(String[] args) {
