fb7e158c4fdc31a77a4f8d5ccf3fa52ffef49ebf
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-10 14:17:28 +0900

    feat: 3계층형 아키텍처 + 인터페이스 v1

---
+++ spring-proxy/src/main/java/com/geunskoo/springproxy/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,6 @@
+package com.geunskoo.springproxy.app.v1;
+
+public interface OrderRepositoryV1 {
+
+    void save(String itemId);
+}
---
+++ spring-proxy/src/main/java/com/geunskoo/springproxy/app/v1/OrderServiceV1.java
@@ -0,0 +1,6 @@
+package com.geunskoo.springproxy.app.v1;
+
+public interface OrderServiceV1 {
+
+    void orderItem(String itemId);
+}
