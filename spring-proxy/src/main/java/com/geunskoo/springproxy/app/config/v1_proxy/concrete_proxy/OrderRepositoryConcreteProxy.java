4c6d2ab4cfb87610097cfb4481eafeb38d6b4191
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-11 23:13:03 +0900

    feat: 인터페이스가 없고 구체 클래스만 있을 경우 프록시 적용 V2

---
+++ spring-proxy/src/main/java/com/geunskoo/springproxy/app/config/v1_proxy/concrete_proxy/OrderRepositoryConcreteProxy.java
@@ -0,0 +1,32 @@
+package com.geunskoo.springproxy.app.config.v1_proxy.concrete_proxy;
+
+import com.geunskoo.springproxy.app.v2.OrderRepositoryV2;
+import com.geunskoo.springproxy.trace.TraceStatus;
+import com.geunskoo.springproxy.trace.logtrace.LogTrace;
+
+public class OrderRepositoryConcreteProxy extends OrderRepositoryV2 {
+
+    private final OrderRepositoryV2 target;
+    private final LogTrace logTrace;
+
+    public OrderRepositoryConcreteProxy(OrderRepositoryV2 target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = logTrace.begin("OrderRepository.request()");
+            //target 호출
+            target.save(itemId);
+            logTrace.end(status);
+
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
